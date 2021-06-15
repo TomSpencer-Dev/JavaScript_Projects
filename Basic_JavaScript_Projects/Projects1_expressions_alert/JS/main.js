a35672409a45a9d943af427df3c2c71b20b2fb0f
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-06-15 08:52:58 -0400

    Assignment Submission #1

---
+++ Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+
+
+
+
+var Hello_World //Defining a variable
+
+Hello_World= "Hello, World!"; //Giving a variable a string value
+
+window.alert(Hello_World); //Executing variable in alert window
+
+
+
+var T = "Happy" + " Birthday", C = "Darren", D="\"Hello World!" + " Have a great day\"";//Defining multiple variables and giving them string values
+var T = T.fontcolor("blue"), C = C.fontcolor("Red"), D= D.fontcolor("Green");//Using the fontcolor method on multiple variables
+
+document.write(D) //Using document.write to display variable
+
+<br>document.write(5*4) //Displaying the result of a multiplication expression</br>
