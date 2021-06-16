6fae694d05cefc022105fc226315c2543d3b3db8
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-06-16 09:29:33 -0400

    Concatenate 2 variables

--- Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
@@ -10,10 +10,10 @@ window.alert(Hello_World); //Executing variable in alert window
 
 
 
-var T = "Happy" + " Birthday", C = "Darren", D="\"Hello World!" + " Have a great day\"";//Defining multiple variables and giving them string values
-var T = T.fontcolor("blue"), C = C.fontcolor("Red"), D= D.fontcolor("Green");//Using the fontcolor method on multiple variables
+var T = "Happy" + " Birthday", C = "Darren", D="Hello World!", E=" Have a great day! ";//Defining multiple variables and giving them string values
+var T = T.fontcolor("blue"), C = C.fontcolor("Red"), D= D.fontcolor("Green"); E=E.fontcolor("yellow")//Using the fontcolor method on multiple variables
 
-document.write(D) //Using document.write to display variable
+document.write(D,E) //Using document.write to display variable
 
 document.write(5*4) //Displaying the result of a multiplication expression
 
