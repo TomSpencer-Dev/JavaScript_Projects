cd6506ac3bfcfd43296a23a20661be98ca1552e3
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-06-16 10:10:08 -0400

    Add Function Multiply

--- Basic_JavaScript_Projects/Projects2_functions/JS/main.js
+++ Basic_JavaScript_Projects/Projects2_functions/JS/main.js
@@ -4,9 +4,14 @@ function Yoga_Function() {                                 //Defining a function
     document.getElementById("asana").innerHTML = result;//Putting the value of result in HTML element with "Asana" id
 }
 
-function myFunction() {      //Degining a function and naming it
+function myFunction() {      //Defining a function and naming it
     var sentence = "Non-Violence can be practised"; //Defining a cariable and giving it a string
     sentence += " both towards others and self;";  //using += operator to concatenate the string
     sentence += " both mentally and physically.";  //using a += operator to concatenate the string
     document.getElementById("Concatenate").innerHTML = sentence; //Putting the value of sentence in HTML element with "Concatenate" id
 }
+
+function Multiply(p1, p2) {  //defining a function and naming it
+    return p1 * p2; //The function returns the product of p1 and p2
+}
+document.getElementById("demo").innerHTML = Multiply(4, 7);
