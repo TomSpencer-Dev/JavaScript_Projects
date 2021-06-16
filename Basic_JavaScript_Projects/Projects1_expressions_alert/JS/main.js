74122d6e688c1bc7beae4aa95c01d987a71b86db
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-06-16 10:45:54 -0400

    Concatenate

--- Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
+++ Basic_JavaScript_Projects/Projects1_expressions_alert/JS/main.js
@@ -13,10 +13,12 @@ window.alert(Hello_World); //Executing variable in alert window
 var T = "Happy" + " Birthday", C = "Darren", D="Hello World!", E=" Have a great day! ";//Defining multiple variables and giving them string values
 var T = T.fontcolor("blue"), C = C.fontcolor("Red"), D= D.fontcolor("Green"); E=E.fontcolor("yellow")//Using the fontcolor method on multiple variables
 
-document.write(D,E) //Using document.write to display variable
+document.write(D + E) //Using document.write to display variable
 
 document.write(5*4) //Displaying the result of a multiplication expression
 
 function displayDate() {
     document.getElementById("demo").innerHTML = Date();
 }
+
+
