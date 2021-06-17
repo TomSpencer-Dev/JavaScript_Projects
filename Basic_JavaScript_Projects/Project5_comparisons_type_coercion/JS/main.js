3344e79dbbef8b781f77fc3c89d39a5db938ca55
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-06-17 13:21:51 -0400

    Add Comparisons

---
+++ Basic_JavaScript_Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,22 @@
+document.write(typeof 3);//document.write method to display number
+
+
+document.write(10==10);
+
+x = 10;
+y = 10;
+document.write (x === y);
+
+
+document.write(2>1 && 3>1);
+
+
+document.write(2>7 || 6<8);
+
+function not_Function() {
+    document.getElementById("Not").innerHTML =! (5>10);
+}
+
+document.write("baseball" + 5)
+
+
