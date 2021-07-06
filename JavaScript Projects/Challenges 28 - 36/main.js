a1584f44b834a93b31fb4f13fe9e6bf86c08cd94
Tom Spencer <84024544+TomSpencer-Dev@users.noreply.github.com>
2021-07-06 11:46:46 -0400

    Add todo folder

---
+++ JavaScript Projects/Challenges 28 - 36/main.js
@@ -0,0 +1,9 @@
+
+
+function openForm() {
+    document.getElementById("myForm1").style.display ="block";
+}
+
+function closeForm() {
+    document.getElementById("myForm1").styledisplay = "none";
+}
